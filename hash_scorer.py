040b75a2a0ec8ce62e56007016752908c4540ac7
Narek Maloyan <narek1110@gmail.com>
2022-11-03 22:48:35 +0300

    pair scores

--- hash_scorer.py
+++ hash_scorer.py
@@ -1,29 +1,50 @@
-from sklearn.metrics import accuracy_score
 import numpy as np
-
-from imagededup.methods import PHash, AHash, DHash, WHash
+from imagededup.methods import AHash, DHash, PHash, WHash
+from sklearn.metrics import accuracy_score
 
 from annotations import annotation
+
 
 def similar_result(hasher, annotation, max_distance_threshold=1):
     results = []
     for i in range(1, 178):
         res = [0] * 12
         try:
-            encodings = hasher.encode_images(image_dir=f'testset/{i}')
-            duplicates = hasher.find_duplicates(encoding_map=encodings, max_distance_threshold=max_distance_threshold)
-            if 'query.jpg' in duplicates:
-                for i in duplicates['query.jpg']:
-                    res[int(i.split('.')[0])] = 1
+            encodings = hasher.encode_images(image_dir=f"testset/{i}")
+            duplicates = hasher.find_duplicates(encoding_map=encodings)
+            if "query.jpg" in duplicates:
+                for i in duplicates["query.jpg"]:
+                    res[int(i.split(".")[0])] = 1
         except:
             pass
         results.append(res)
 
-    return accuracy_score(np.array(annotation).flatten(), np.array(results).flatten())
+    return (
+        results,
+        accuracy_score(np.array(annotation).flatten(), np.array(results).flatten()),
+    )
+
 
 final = {}
+final_acc = {}
 for i in [PHash, AHash, DHash, WHash]:
-    for max_distance in [1, 2]:
-        final[f"{i.__name__}_{max_distance}"] = similar_result(i(verbose=False), annotation, max_distance)
+    # for max_distance in [1, 2, 4]:
+    res, acc = similar_result(i(verbose=False), annotation)
+    final[f"{i.__name__}"] = res
+    final_acc[f"{i.__name__}"] = acc
 
-print(final)
+print(final_acc)
+
+for i in [PHash, AHash, DHash, WHash]:
+    for j in [PHash, AHash, DHash, WHash]:
+        if i != j:
+            print(
+                f"{i.__name__} + {j.__name__}",
+                accuracy_score(
+                    np.array(annotation).flatten(),
+                    np.array(
+                        np.array(final[f"{i.__name__}"])
+                        | np.array(final[f"{j.__name__}"])
+                    ).flatten(),
+                )
+            )
